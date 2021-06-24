f80f4e75eb975b03f46f06192ef32770edda61da
Ceres-VIII <ceres_viii@outlook.com>
2021-06-24 16:28:18 +0300

    %

--- js/credit.js
+++ js/credit.js
@@ -7,11 +7,11 @@ function calc(){
     let credit = sum*time*rate+sum;
     let pay = credit/time;
     console.log(credit);
-    credittable.innerHTML ="<tr><th class='month'>Місяць</th><th>Борг</th><th>До сплати</th></tr>";
+    credittable.innerHTML ="<tr><th class='month'>Місяць</th><th>Борг</th><th>Відсотки</th><th>До сплати</th></tr>";
     for (let i = 1; i <= time; i++){
         credit = Math.round(credit);
         pay = Math.round(pay);
-        credittable.innerHTML +=`<tr><td class="month">${i}</td><td>${credit} грн</td><td>${pay} грн</td></tr>`;
+        credittable.innerHTML +=`<tr><td class="month">${i}</td><td>${credit} грн</td><td>${Math.round(sum*rate)} грн</td><td>${pay} грн</td></tr>`;
         credit = credit - pay;
     }
 
