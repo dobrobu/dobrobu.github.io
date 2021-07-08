ea280f6ea2e61a172cc04aa06e1582ecd24eac99
ksdobrobut <ksdobrobut.site@gmail.com>
2021-07-08 16:46:31 +0300

    publick

--- js/credit.js
+++ js/credit.js
@@ -10,7 +10,7 @@ function calc(){
 
         credit = Math.round(credit);
         pay = Math.round(pay);
-        outplace.innerHTML='<div class="mt-2"><p class="text-info">Щомісячний платіж:</p><span class="fs-3"  id="payout" ></span> грн</div><div class="mt-2"><p class="text-info" for="sumh">Відсотки:</p><span class="fs-3"   id="sumout"></span> грн</div>'
+        outplace.innerHTML='<div class="mt-2"><p class="text-info" for="sumh">Відсотки:</p><span class="fs-3"   id="sumout"></span> грн</div> <div class="mt-2"><p class="text-info">Щомісячний платіж:</p><span class="fs-3"  id="payout" ></span> грн</div>'
         payout.innerHTML = pay;
         sumout.innerHTML = Math.round(sum*time*rate);
 
